dd3f8e565f1b82cc5432378c3b36d19e05993a47
Taylor Shin <t2shin@ucsd.edu>
2016-09-15 01:28:32 -0700

    added buttons and labels

--- robot.py
+++ robot.py
@@ -1,3 +1,41 @@
+class Robot:
+  def __init__(self, canvas, speed, color):
+    self.canvas = canvas
+    self.id = canvas.create_oval(10, 10, 50, 50, fill=color)
+    # Sets x,y position of object
+    self.canvas.move(self.id, 300, 300)
+    self.speed = speed
+    self.x = 0
+    self.y = speed
+    self.canvas_height = self.canvas.winfo_reqheight()
+    self.canvas_width = self.canvas.winfo_reqwidth()
+    self.gravity = 0.05
+
+  def draw(self):
+    self.canvas.move(self.id, self.x, self.y)
+    pos = self.canvas.coords(self.id)
+    if pos[1] <= 0:
+      #self.y = self.speed
+      self.y = abs(self.y)
+    if pos[3] >= self.canvas_height:
+      self.y = -self.speed
+    if pos[0] <= 0:
+      self.x = 0
+    if pos[2] >= self.canvas_width:
+      self.x = 0
+    self.y += self.gravity
+
+class Wall:
+  def __init__(self, canvas, color):
+    self.canvas = canvas
+    # (x0, y0, x1, y1) are parameters for create_rectangle
+    # (x0, y0) = top left point; (x1, y1) = bottom right point
+    self.id = canvas.create_rectangle(400, 200, 425, 550, fill=color)
+    #self.canvas.move(self.id, 200, 300)
+
+  def draw(self):
+    self.canvas.move(self.id, 0, 0)
+
 if __name__ == '__main__':
     from robotDisplay import *
     run()
--- robotDisplay.py
+++ robotDisplay.py
@@ -3,46 +3,34 @@ import threading
 import time
 import sys
 import random
-
-class Robot:
-
-  def __init__(self, canvas, speed, color):
-    self.canvas = canvas
-    self.id = canvas.create_oval(10, 10, 25, 25, fill=color)
-    # Sets x,y position of object
-    self.canvas.move(self.id, 300, 300)
-    self.speed = speed
-    self.x = 0
-    self.y = speed
-    self.canvas_height = self.canvas.winfo_reqheight()
-    self.canvas_width = self.canvas.winfo_reqwidth()
-
-  def draw(self):
-    self.canvas.move(self.id, self.x, self.y)
-    pos = self.canvas.coords(self.id)
-    if pos[1] <= 0:
-      self.y = self.speed
-    if pos[3] >= self.canvas_height:
-    #if pos[3] >= 500:
-      self.y = -self.speed
-    if pos[0] <= 0:
-      self.x = 0
-    if pos[2] >= self.canvas_width:
-      self.x = 0
-
-class Wall:
-
-  def __init__(self, canvas, color):
-    self.canvas = canvas
-    # (x0, y0, x1, y1) are parameters for create_rectangle
-    # (x0, y0) = top left point; (x1, y1) = bottom right point
-    self.id = canvas.create_rectangle(400, 200, 450, 550, fill=color)
-    #self.canvas.move(self.id, 200, 300)
-    
-  def draw(self):
-    self.canvas.move(self.id, 0, 0)
+import robot
 
 class Application:
+
+  def sigmoid(self, x):
+      return 1.0 / (1.0 + 2.0 ** (-x))
+
+  def incrementSpeed(self, inc):
+      self.tickTime *= inc
+      self.speed_label['text'] = 'Step Delay: %.5f' % (self.tickTime)
+
+  def incrementEpsilon(self, inc):
+      self.ep += inc
+      self.epsilon = self.sigmoid(self.ep)
+      #self.learner.setEpsilon(self.epsilon)
+      self.epsilon_label['text'] = 'Epsilon: %.3f' % (self.epsilon)
+
+  def incrementGamma(self, inc):
+      self.ga += inc
+      self.gamma = self.sigmoid(self.ga)
+      #self.learner.setDiscount(self.gamma)
+      self.gamma_label['text'] = 'Discount: %.3f' % (self.gamma)
+
+  def incrementAlpha(self, inc):
+      self.al += inc
+      self.alpha = self.sigmoid(self.al)
+      #self.learner.setLearningRate(self.alpha)
+      self.alpha_label['text'] = 'Learning Rate: %.3f' % (self.alpha)
 
   def initGUI(self, win):
     # Window
@@ -54,19 +42,82 @@ class Application:
     self.inc = .5
     self.tickTime = 0.1
 
+    # Buttons and labels
+    self.setupSpeedButtonAndLabel(win)
+    self.setupEpsilonButtonAndLabel(win)
+    self.setUpGammaButtonAndLabel(win)
+    self.setupAlphaButtonAndLabel(win)
+
     # Canvas
     self.canvas = Tkinter.Canvas(root, height=500, width=800)
     self.canvas.grid(row=2, columnspan=10)
 
+  def setupAlphaButtonAndLabel(self, win):
+      self.alpha_minus = Tkinter.Button(win,
+      text="-",command=(lambda: self.incrementAlpha(self.dec)))
+      self.alpha_minus.grid(row=1, column=3, padx=10)
+
+      self.alpha = self.sigmoid(self.al)
+      self.alpha_label = Tkinter.Label(win, text='Learning Rate: %.3f' % (self.alpha))
+      self.alpha_label.grid(row=1, column=4)
+
+      self.alpha_plus = Tkinter.Button(win,
+      text="+",command=(lambda: self.incrementAlpha(self.inc)))
+      self.alpha_plus.grid(row=1, column=5, padx=10)
+
+  def setUpGammaButtonAndLabel(self, win):
+      self.gamma_minus = Tkinter.Button(win,
+      text="-",command=(lambda: self.incrementGamma(self.dec)))
+      self.gamma_minus.grid(row=1, column=0, padx=10)
+
+      self.gamma = self.sigmoid(self.ga)
+      self.gamma_label = Tkinter.Label(win, text='Discount: %.3f' % (self.gamma))
+      self.gamma_label.grid(row=1, column=1)
+
+      self.gamma_plus = Tkinter.Button(win,
+      text="+",command=(lambda: self.incrementGamma(self.inc)))
+      self.gamma_plus.grid(row=1, column=2, padx=10)
+
+  def setupEpsilonButtonAndLabel(self, win):
+      self.epsilon_minus = Tkinter.Button(win,
+      text="-",command=(lambda: self.incrementEpsilon(self.dec)))
+      self.epsilon_minus.grid(row=0, column=3)
+
+      self.epsilon = self.sigmoid(self.ep)
+      self.epsilon_label = Tkinter.Label(win, text='Epsilon: %.3f' % (self.epsilon))
+      self.epsilon_label.grid(row=0, column=4)
+
+      self.epsilon_plus = Tkinter.Button(win,
+      text="+",command=(lambda: self.incrementEpsilon(self.inc)))
+      self.epsilon_plus.grid(row=0, column=5)
+
+  def setupSpeedButtonAndLabel(self, win):
+      self.speed_minus = Tkinter.Button(win,
+      text="-",command=(lambda: self.incrementSpeed(.5)))
+      self.speed_minus.grid(row=0, column=0)
+
+      self.speed_label = Tkinter.Label(win, text='Step Delay: %.5f' % (self.tickTime))
+      self.speed_label.grid(row=0, column=1)
+
+      self.speed_plus = Tkinter.Button(win,
+      text="+",command=(lambda: self.incrementSpeed(2)))
+      self.speed_plus.grid(row=0, column=2)
+
+  def skip5kSteps(self):
+    self.stepsToSkip = 5000
+
   def __init__(self, win):
+    self.ep = 0
+    self.ga = 2
+    self.al = 2
     self.stepCount = 0
 
     # Initialize GUI
     self.initGUI(win)
 
     # Initialize environment
-    self.robot = Robot(self.canvas, 3, 'blue')
-    self.wall = Wall(self.canvas, 'black')
+    self.robot = robot.Robot(self.canvas, 3, 'blue')
+    self.wall = robot.Wall(self.canvas, 'black')
 
     # Start GUI
     self.running = True
@@ -102,6 +153,10 @@ class Application:
       if not self.running:
         self.stopped = True
         return
+      for i in range(self.stepsToSkip):
+        self.step()
+      self.stepsToSkip = 0
+      self.step()
 
   def start(self):
     self.win.mainloop()
